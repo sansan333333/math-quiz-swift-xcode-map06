47f25406e8c820b4fa3aea646c2e523b070c8930
sansan333333 <71362622+sansan333333@users.noreply.github.com>
2022-09-30 14:58:06 -0400

    Add files via upload

---
+++ math quiz/math quiz/firstPage.swift
@@ -0,0 +1,17 @@
+//
+//  firstPage.swift
+//  math quiz
+//
+//  Created by Jun on 2022-09-28.
+//
+
+import UIKit
+
+class firstPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ math quiz/math quiz/result.swift
@@ -0,0 +1,14 @@
+//
+//  resule.swift
+//  math quiz
+//
+//  Created by Jun on 2022-09-29.
+//
+
+import Foundation
+
+struct Result{
+    var yourQuestion: String
+    var yourAnswer: String
+    var rightOrWrong: String
+}
